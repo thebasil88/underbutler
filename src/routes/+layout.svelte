b1a1366f9b805ec18746350374ef7d29c757d67b
Nicholas Basil <thebasil88@gmail.com>
2023-01-05 09:35:45 -0500

    installed supabase and created auth components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,19 @@
-<script>
+<script lang="ts">
+    import { supabase } from '$lib/supabaseClient'
+    import { invalidate } from '$app/navigation'
+    import { onMount } from 'svelte'
+
+    onMount(() => {
+        const {
+        data: { subscription },
+        } = supabase.auth.onAuthStateChange(() => {
+        invalidate('supabase:auth')
+        })
+
+        return () => {
+        subscription.unsubscribe()
+        }
+    })
     import "../app.css";
 </script>
 <div data-theme="luxury">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script>
-    import Button from "../lib/daisyui/actions/Button.svelte";
+   
+    import Account from '$lib/supabase/Account.svelte'
+    import Auth from '$lib/supabase/Auth.svelte'
+    import Button from "$lib/daisyui/actions/Button.svelte";
+    import NavBar from '$lib/daisyui/navigation/NavBar.svelte';
 </script>
+<svelte:head>
+  <title>Supabase + SvelteKit</title>
+  <meta name="description" content="SvelteKit using supabase-js v2" />
+</svelte:head>
+
+<NavBar />
+
 <h1 class="text-3xl font-bold underline">
     Hello world!
 </h1>
